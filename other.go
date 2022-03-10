3e81a048d925f26f5361060998aaf4db41376e05
akitanak <akihito_tanaka@me.com>
2022-03-10 23:19:52 +0900

    add ForEach

--- other.go
+++ other.go
@@ -1,5 +1,11 @@
 package collectionsutil
 
+func ForEach[T any](s []T, fn func(e T)) {
+	for _, e := range s {
+		fn(e)
+	}
+}
+
 func Reverse[T any](l []T) []T {
 	reversed := make([]T, len(l))
 	lastIndex := len(l) - 1
--- other_test.go
+++ other_test.go
@@ -4,6 +4,27 @@ import (
 	"testing"
 )
 
+func Test_ForEach(t *testing.T) {
+	t.Run("doubled", func(t *testing.T) {
+		in := []int{1, 2, 3, 4, 5}
+		want := []int{2, 4, 6, 8, 10}
+		recorder := make([]int, len(in))
+		counter := 0
+		fn := func(e int) {
+			recorder[counter] = e * 2
+			counter++
+		}
+
+		ForEach(in, fn)
+
+		for i, e := range recorder {
+			if e != want[i] {
+				t.Errorf("expected %d at index %d, but got %d", want[i], i, e)
+			}
+		}
+	})
+}
+
 func Test_Reverse(t *testing.T) {
 	testCase := map[string]struct {
 		in   []int
