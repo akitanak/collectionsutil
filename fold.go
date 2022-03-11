3bd01c0c6343e063a1d976961c3c21e8d115062f
akitanak <akihito_tanaka@me.com>
2022-03-12 00:18:16 +0900

    add ReduceRight

--- fold.go
+++ fold.go
@@ -2,30 +2,42 @@ package collectionsutil
 
 import "fmt"
 
-func FoldLeft[IN, OUT any](l []IN, z OUT, fn func(acc OUT, in IN) OUT) OUT {
+func FoldLeft[IN, OUT any](s []IN, z OUT, fn func(acc OUT, in IN) OUT) OUT {
 	acc := z
-	for _, v := range l {
+	for _, v := range s {
 		acc = fn(acc, v)
 	}
 	return acc
 }
 
-func FoldRight[IN, OUT any](l []IN, z OUT, fn func(in IN, acc OUT) OUT) OUT {
+func FoldRight[IN, OUT any](s []IN, z OUT, fn func(in IN, acc OUT) OUT) OUT {
 	acc := z
-	for i := len(l); i > 0; i-- {
-		acc = fn(l[i-1], acc)
+	for i := len(s); i > 0; i-- {
+		acc = fn(s[i-1], acc)
 	}
 	return acc
 }
 
-func ReduceLeft[T any](l []T, fn func(acc T, in T) T) (T, error) {
+func ReduceLeft[T any](s []T, fn func(acc T, in T) T) (T, error) {
 	var acc T
-	if len(l) < 1 {
-		return acc, fmt.Errorf("l length must be more than 1. but got %d", len(l))
+	if len(s) < 1 {
+		return acc, fmt.Errorf("s length must be more than 1. but got %d", len(s))
 	}
-	acc = l[0]
-	for i := 1; i < len(l); i++ {
-		acc = fn(acc, l[i])
+	acc = s[0]
+	for i := 1; i < len(s); i++ {
+		acc = fn(acc, s[i])
+	}
+	return acc, nil
+}
+
+func ReduceRight[T any](s []T, fn func(in T, acc T) T) (T, error) {
+	var acc T
+	if len(s) < 1 {
+		return acc, fmt.Errorf("s length must be more than 1. but got %d", len(s))
+	}
+	acc = s[len(s)-1]
+	for i := len(s) - 1; i > 0; i-- {
+		acc = fn(s[i-1], acc)
 	}
 	return acc, nil
 }
--- fold_test.go
+++ fold_test.go
@@ -95,9 +95,58 @@ func Test_ReduceLeft(t *testing.T) {
 
 	t.Run("if input empty slice, got error.", func(t *testing.T) {
 		in := []int{}
-		wantErrMsg := "l length must be more than 1. but got 0"
+		wantErrMsg := "s length must be more than 1. but got 0"
 
 		got, err := ReduceLeft(in, func(acc int, in int) int {
+			return acc - in
+		})
+		if got != 0 {
+			t.Errorf("expect zero value(0), but got %d", got)
+		}
+		if err.Error() != wantErrMsg {
+			t.Errorf("expect error message %s, but got %s", wantErrMsg, err.Error())
+		}
+	})
+}
+
+func Test_ReduceRight(t *testing.T) {
+	t.Run("subtract integers", func(t *testing.T) {
+		in := []int{1, 2, 3, 4, 5}
+		want := -5
+
+		got, err := ReduceRight(in, func(in int, acc int) int {
+			return acc - in
+		})
+		if err != nil {
+			t.Errorf("expect no error, but got %v", err)
+		}
+
+		if got != want {
+			t.Errorf("expect %d, but got %d", want, got)
+		}
+	})
+
+	t.Run("subtract integers(1 value)", func(t *testing.T) {
+		in := []int{1}
+		want := 1
+
+		got, err := ReduceRight(in, func(in int, acc int) int {
+			return acc - in
+		})
+		if err != nil {
+			t.Errorf("expect no error, but got %v", err)
+		}
+
+		if got != want {
+			t.Errorf("expect %d, but got %d", want, got)
+		}
+	})
+
+	t.Run("if input empty slice, got error.", func(t *testing.T) {
+		in := []int{}
+		wantErrMsg := "s length must be more than 1. but got 0"
+
+		got, err := ReduceRight(in, func(in int, acc int) int {
 			return acc - in
 		})
 		if got != 0 {
