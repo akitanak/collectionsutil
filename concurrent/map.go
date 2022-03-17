23c56b815bd5a173e72b6328387a50e4bf3a8151
akitanak <akihito_tanaka@me.com>
2022-03-18 00:04:17 +0900

    add concurrent FlatMap

--- concurrent/map.go
+++ concurrent/map.go
@@ -2,10 +2,10 @@ package concurrent
 
 import "sync"
 
-func Map[IN, OUT any](l []IN, fn func(v IN) OUT) []OUT {
-	mapped := make([]OUT, len(l))
+func Map[IN, OUT any](s []IN, fn func(v IN) OUT) []OUT {
+	mapped := make([]OUT, len(s))
 	wg := sync.WaitGroup{}
-	for i, v := range l {
+	for i, v := range s {
 		i, v := i, v
 		wg.Add(1)
 		go func() {
@@ -15,4 +15,26 @@ func Map[IN, OUT any](l []IN, fn func(v IN) OUT) []OUT {
 	}
 	wg.Wait()
 	return mapped
+}
+
+func FlatMap[IN, OUT any](s []IN, fn func(v IN) []OUT) []OUT {
+	mutex := &sync.Mutex{}
+	mapped := make(map[int][]OUT, 0)
+	wg := sync.WaitGroup{}
+	for i, v := range s {
+		i, v := i, v
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mutex.Lock()
+			mapped[i] = fn(v)
+			mutex.Unlock()
+		}()
+	}
+	wg.Wait()
+	flatten := make([]OUT, 0)
+	for i := 0; i < len(s); i++ {
+		flatten = append(flatten, mapped[i]...)
+	}
+	return flatten
 }
--- concurrent/map_test.go
+++ concurrent/map_test.go
@@ -1,11 +1,14 @@
 package concurrent
 
 import (
+	"errors"
+	"fmt"
 	"strconv"
+	"strings"
 	"testing"
 )
 
-func Test_Map(t *testing.T) {
+func Test_ConcurrentMap(t *testing.T) {
 	t.Run("int squaring", func(t *testing.T) {
 		t.Parallel()
 
@@ -46,6 +49,54 @@ func Test_Map(t *testing.T) {
 		for i, v := range result {
 			if v != want[i] {
 				t.Errorf("mapped value(index: %d) is wrong. want: %v, got: %v", i, want[i], v)
+			}
+		}
+	})
+}
+
+type Either[T any] struct {
+	right T
+	left  error
+}
+
+func Test_ConcurrentFlatMap(t *testing.T) {
+	t.Run("extract comma values list", func(t *testing.T) {
+		t.Parallel()
+
+		in := []string{
+			"1,2,3,4,5",
+			"6,7,eight,9,10",
+		}
+		fn := func(v string) []Either[int] {
+			mapped := make([]Either[int], 0)
+			for _, v := range strings.Split(v, ",") {
+				i, err := strconv.Atoi(v)
+				if err != nil {
+					mapped = append(mapped, Either[int]{left: err})
+					continue
+				}
+				mapped = append(mapped, Either[int]{right: i})
+			}
+			return mapped
+		}
+		want := []Either[int]{
+			{right: 1}, {right: 2}, {right: 3}, {right: 4}, {right: 5},
+			{right: 6}, {right: 7}, {left: fmt.Errorf("strconv.Atoi: parsing \"eight\": invalid syntax")}, {right: 9}, {right: 10},
+		}
+
+		result := FlatMap(in, fn)
+
+		if len(result) != len(want) {
+			t.Errorf("result length is expected %d, but got %d", len(want), len(result))
+		}
+
+		for i, v := range result {
+			if want[i].right != 0 && v.right != want[i].right {
+				t.Errorf("result right(index: %d) is expected %v, but got %v", i, want[i].right, v.right)
+			}
+
+			if want[i].left != nil && errors.Is(v.left, want[i].left) {
+				t.Errorf("result left(index: %d) is expected %v, but got %v", i, want[i].left, v.left)
 			}
 		}
 	})
